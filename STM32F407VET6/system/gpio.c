6e0318e067d6e194e576d3324ddc9cbfd4b5b6df
LHYLiuWilliam <LHYLiuWilliam@gmail.com>
2024-08-04 19:37:23 +0800

    Add gpio driver

---
+++ STM32F407VET6/system/gpio.c
@@ -0,0 +1,24 @@
+#include <string.h>
+
+#include "gpio.h"
+
+void GPIO_init(GPIO *gpio) {
+  char *temp = gpio->GPIOxPiny;
+
+  uint8_t count = 0;
+  do {
+    gpio->GPIOx[count] = GPIOx(temp);
+    gpio->Pin[count] = GPIO_Pinx(temp);
+
+    HAL_RCC_GPIOx_CLK_ENABLE(gpio->GPIOx[count]);
+
+    GPIO_InitTypeDef GPIO_Init = {
+        .Pin = gpio->Pin[count],
+        .Mode = gpio->Mode[count],
+        .Pull = gpio->Pull[count],
+        .Speed = GPIO_SPEED_FREQ_HIGH,
+    };
+    HAL_GPIO_Init(gpio->GPIOx[count], &GPIO_Init);
+
+  } while ((temp = strchr(temp, '|')) && (temp = temp + 2) && (count++));
+}
