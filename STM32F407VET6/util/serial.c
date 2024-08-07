248bbe6e29c39c7731b25364113aecfe4c52795c
LHYLiuWilliam <LHYLiuWilliam@gmail.com>
2024-08-07 15:58:44 +0800

    Add serial driver

---
+++ STM32F407VET6/util/serial.c
@@ -0,0 +1,38 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "serial.h"
+
+void Serial_init(Serial *serial) {
+    serial->Handler = (UART_HandleTypeDef){
+        .Instance = serial->usart,
+        .Init =
+            {
+                .BaudRate = serial->baudrate,
+                .WordLength = UART_WORDLENGTH_8B,
+                .Parity = UART_PARITY_NONE,
+                .StopBits = UART_STOPBITS_1,
+                .Mode = UART_MODE_TX_RX,
+            },
+    };
+    HAL_UART_Init(&serial->Handler);
+}
+
+void Serial_RXITStrart(Serial *serial, uint8_t size) {
+    HAL_UART_Receive_IT(&serial->Handler, serial->RXBuffer, size);
+}
+
+void Serial_sendBytes(Serial *serial, uint8_t *bytes, uint8_t length) {
+    HAL_UART_Transmit(&serial->Handler, bytes, length, 0xFFFF);
+}
+
+void Serial_printf(Serial *serial, char *format, ...) {
+    va_list arg;
+    va_start(arg, format);
+    vsprintf((char *)serial->TXBuffer, format, arg);
+    va_end(arg);
+
+    HAL_UART_Transmit(&serial->Handler, (uint8_t *)serial->TXBuffer,
+                      strlen((char *)serial->TXBuffer), 0xFFFF);
+}
---
+++ STM32F407VET6/util/serial.h
@@ -0,0 +1,52 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+#include "stm32f4xx.h"
+
+#define __HAL_RCC_USARTx_CLK_ENABLE(x)                                         \
+    do {                                                                       \
+        if ((x) == USART1) {                                                   \
+            __HAL_RCC_USART1_CLK_ENABLE();                                     \
+        } else if ((x) == USART2) {                                            \
+            __HAL_RCC_USART2_CLK_ENABLE();                                     \
+        } else if ((x) == USART3) {                                            \
+            __HAL_RCC_USART3_CLK_ENABLE();                                     \
+        }                                                                      \
+    } while (0)
+
+#define GPIO_AF7_USARTx(x)                                                     \
+    ((x) == USART1   ? GPIO_AF7_USART1                                         \
+     : (x) == USART2 ? GPIO_AF7_USART2                                         \
+     : (x) == USART3 ? GPIO_AF7_USART3                                         \
+                     : NULL)
+
+#define USARTx_IRQn(x)                                                         \
+    ((x) == USART1   ? USART1_IRQn                                             \
+     : (x) == USART2 ? USART2_IRQn                                             \
+     : (x) == USART3 ? USART3_IRQn                                             \
+                     : NULL)
+
+#define TXBUFFER_SIZE 128
+#define RXBUFFER_SIZE 128
+
+typedef struct {
+    USART_TypeDef *usart;
+
+    char RX[4];
+    char TX[4];
+
+    uint32_t baudrate;
+
+    uint8_t TXBuffer[TXBUFFER_SIZE];
+    uint8_t RXBuffer[RXBUFFER_SIZE];
+
+    UART_HandleTypeDef Handler;
+} Serial;
+
+void Serial_init(Serial *serial);
+void Serial_RXITStrart(Serial *serial, uint8_t size);
+
+void Serial_sendBytes(Serial *serial, uint8_t *bytes, uint8_t length);
+void Serial_printf(Serial *serial, char *format, ...);
+
+#endif
